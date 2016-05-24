1f9a217a2bcd9a034b4bfaf525f5fc5455e2337a
tobiasviehweger <tobias.viehweger@gmx.de>
2016-05-24 16:43:20 +0200

    jquery 3 readiness

--- jquery.mentionsInput.js
+++ jquery.mentionsInput.js
@@ -108,15 +108,15 @@
             elmWrapperBox = elmInputWrapper.find('> div.mentions-input-box'); //Obtains the div elmWrapperBox that now contains the text area
 
             elmInputBox.attr('data-mentions-input', 'true'); //Sets the attribute data-mentions-input to true -> Defines if the text area is already configured
-            elmInputBox.bind('keydown', onInputBoxKeyDown); //Bind the keydown event to the text area
-            elmInputBox.bind('keypress', onInputBoxKeyPress); //Bind the keypress event to the text area
-            elmInputBox.bind('click', onInputBoxClick); //Bind the click event to the text area
-            elmInputBox.bind('blur', onInputBoxBlur); //Bind the blur event to the text area
+            elmInputBox.on('keydown', onInputBoxKeyDown); //Bind the keydown event to the text area
+            elmInputBox.on('keypress', onInputBoxKeyPress); //Bind the keypress event to the text area
+            elmInputBox.on('click', onInputBoxClick); //Bind the click event to the text area
+            elmInputBox.on('blur', onInputBoxBlur); //Bind the blur event to the text area
 
             if (navigator.userAgent.indexOf("MSIE 8") > -1) {
-                elmInputBox.bind('propertychange', onInputBoxInput); //IE8 won't fire the input event, so let's bind to the propertychange
+                elmInputBox.on('propertychange', onInputBoxInput); //IE8 won't fire the input event, so let's bind to the propertychange
             } else {
-                elmInputBox.bind('input', onInputBoxInput); //Bind the input event to the text area
+                elmInputBox.on('input', onInputBoxInput); //Bind the input event to the text area
             }
 
             // Elastic textareas, grow automatically
